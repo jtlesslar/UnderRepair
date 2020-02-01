66c28c5a2ac9c197b8509c2537f17e822a44933f
Jamboger <jamboger@gmail.com>
2020-02-01 14:50:33 +0000

    implemented multiple controllers

---
+++ Assets/player2Input.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player2Input : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float deadZone = 0.2f;
+    public string horizontalAxisName = "Joy3Horz";
+    public string verticalAxisName = "Joy3Vert";
+    public float speed = 3;
+
+    private void Update()
+    {
+        Vector3 flatForward = Camera.main.transform.forward;
+        flatForward.y = 0;
+        flatForward.Normalize();
+
+        Vector3 flatRight = Camera.main.transform.right;
+        flatRight.y = 0;
+        flatRight.Normalize();
+
+        if (Mathf.Abs(Input.GetAxis(horizontalAxisName)) > deadZone)
+            gameObject.transform.Translate(flatRight * Input.GetAxis(horizontalAxisName) * Time.deltaTime * speed);
+        if (Mathf.Abs(Input.GetAxis(verticalAxisName)) > deadZone)
+            gameObject.transform.Translate(flatForward * -Input.GetAxis(verticalAxisName) * Time.deltaTime * speed);
+    }
+}
