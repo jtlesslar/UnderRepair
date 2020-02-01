30a02031370c5d0845ebb7bc396d5fa986f9f5ab
James Lesslar <jlesslar@hotmail.co.uk>
2020-02-01 23:06:07 +0000

    Add Raycasting to allow player to interact with objects.

---
+++ Assets/Scripts/MaterialPickup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class MaterialPickup : Interactable
+{  
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        Pickup();
+    }
+
+    void Pickup()
+    {
+        Debug.Log("Picking up material");
+
+    }
+}
+
