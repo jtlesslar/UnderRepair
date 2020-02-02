b3411f23cb457ec081f3ed486d57497af5247a58
Liam <liamfields116@gmail.com>
2020-02-02 11:56:53 +0000

    spawner

--- Assets/Scipts/Spawner.cs
+++ Assets/Scipts/Spawner.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Spawner : MonoBehaviour{
+
+
+    public Transform[] spawnLocations;
+    public GameObject[] whatToSpawnPrefab;
+    public GameObject[] whatToSpawnClone; 
+
+
+    void start(){
+        spawnMaterialsToolsPlease();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void spawnMaterialsToolsPlease(){
+        whatToSpawnClone[0] = Instantiate(whatToSpawnPrefab[0],spawnLocations[0].transform.position, Quaternion.Euler(0,0,0)) as GameObject;
     }
+
 }
